e96b3dda9d4e4b876770c980a39b435e3e60f3c7
kyan <kyanarifay@gmail.com>
2025-09-19 11:19:48 +0700

    two commit

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "xirpl2-32"; // default user XAMPP
 $pass = "3089614104";     // default password kosong
-$db   = " db_xirpl2-32_1  ";
+$db   = "db_xirpl2-32_1  ";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
